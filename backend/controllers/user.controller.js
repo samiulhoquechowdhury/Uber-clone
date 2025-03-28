b0d008f2eed4eb911c60083022eb4607e3ba3857
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-03-28 16:09:45 +0530

    logic: User register controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -7,4 +7,25 @@ module.exports.registerUser = async (req, res, next) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+
+  const { fullname, email, password } = req.body;
+
+  const isUserAlready = await userModel.findOne({ email });
+
+  if (isUserAlready) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+
+  const hashedPassword = await userModel.hashPassword(password);
+
+  const user = await userService.createUser({
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = user.generateAuthToken();
+
+  res.status(201).json({ token, user });
 };
